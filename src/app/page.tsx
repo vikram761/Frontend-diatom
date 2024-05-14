bd02124dfd1f8ad8f70f0a5f0bfe8726a2427027
vikram <srimanvgn@gmail.com>
2024-05-14 22:35:38 +0530

    fix : build bug fixed
    Changes:
    - added multiple background (dna and neuron)
    - create new directory for backgrounds inside public
    - fixed ' which prevent `npm run build`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <Hero/>
       <div className="2xl:px-80 lg:px-28  md:px-24 px-6 py-16 w-full ">
-        <h1 className="lg:text-3xl text-2xl font-medium max-w-4xl lg:leading-10 ">We're DiAtom Technologies. At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate</h1>
+        <h1 className="lg:text-3xl text-2xl font-medium max-w-4xl lg:leading-10 ">We are DiAtom Technologies. At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate</h1>
       </div>
       <div className="h-screen w-full"></div>
     </>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <div className={cn("-z-50 lg:h-[70vh] h-[93vh] relative",openSans.className)}>
       <div className="w-full h-full bg-ovelay_black absolute top-0 -z-30"></div>
       <video autoPlay loop muted className="w-full h-full object-cover absolute -z-40">
-        <source src="/background.mp4" type="video/mp4" />
+        <source src="/bg/background2.mp4" type="video/mp4" />
       </video>
       <div className="2xl:px-80 lg:px-28  md:px-24 px-8 h-full py-20 flex items-end max-lg:items-center ">
         <h2 className="lg:text-7xl md:text-6xl text-4xl text-white font-semibold lg:max-w-4xl ">Applying the power of Science and Technology</h2>
